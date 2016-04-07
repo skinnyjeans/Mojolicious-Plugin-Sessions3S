50f1dd7150fd43bfe9a2737f8adf1075e17e4232
Jerome Eteve <jerome@broadbean.com>
2016-04-07 12:14:32 +0100

    Added missing doc

--- lib/Mojolicious/Sessions/ThreeS/Storage/Memory.pm
+++ lib/Mojolicious/Sessions/ThreeS/Storage/Memory.pm
@@ -2,17 +2,41 @@ package Mojolicious::Sessions::ThreeS::Storage::Memory;
 
 use Mojo::Base qw/Mojolicious::Sessions::ThreeS::Storage/;
 
+=head1 NAME
+
+Mojolicious::Sessions::ThreeS::Storage::Memory - A simple in Memory storage for sessions. Nice for tests.
+
+=cut
+
 my $_SESSION_STORE = {};
+
+=head2 get_session
+
+See L<Mojolicious::Sessions::ThreeS::Storage>
+
+=cut
 
 sub get_session{
     my ($self, $session_id) = @_;
     return $_SESSION_STORE->{$session_id};
 }
 
+=head2 store_session
+
+See L<Mojolicious::Sessions::ThreeS::Storage>
+
+=cut
+
 sub store_session{
     my ($self, $session_id, $session) = @_;
     $_SESSION_STORE->{$session_id} = $session;
 }
+
+=head2 remove_session_id
+
+See L<Mojolicious::Sessions::ThreeS::Storage>
+
+=cut
 
 sub remove_session_id{
     my ($self, $session_id) = @_;
